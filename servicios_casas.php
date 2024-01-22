8cb882c16bc10e99a8114e85c728cd6e581604e1
PEISA203Anaya <157181455+PEISA203Anaya@users.noreply.github.com>
2024-01-22 14:40:59 -0600

    Add files via upload

---
+++ servicios_casas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servicios</title>
+    <link rel="stylesheet" href="styleServices.css">
+</head>
+<body>
+    <section class="services">
+        <div class="service">
+        <img src="Images/Casalujosa1.jpg" alt="Casa 1">
+            <h2>Casa Moderna</h2>
+            <p>Descubre nuestras casas modernas con diseño elegante y comodidades de vanguardia.</p>
+        </div>
+        
+        <div class="service">
+            <img src="Images/Casalujosa3.jpg" alt="Casa 2">
+            <h2>Vistas Panorámicas</h2>
+            <p>Disfruta de vistas panorámicas con nuestras casas ubicadas en entornos naturales impresionantes.</p>
+        </div>
+        
+        <div class="service">
+            <img src="Images/Casalujosa4.jpg" alt="Casa 3">
+            <h2>Casas de Lujo</h2>
+            <p>Encuentra la casa de lujo de tus sueños con características exclusivas y acabados de alta calidad.</p>
+        </div>
+    </section>
+    <a href="PagWeb.php" class="btn-volver">Volver a la Página Principal</a>
+</body>
+</html>
